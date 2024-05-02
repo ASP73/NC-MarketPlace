94e2b72459f24c9b574e67c3ecbe547053ea5cc1
湾仔 <Wendy@shiwanwandembp.home>
2024-05-02 08:44:56 +0100

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
-
 import "./App.css";
+import Header from "./Components/Header";
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<Header />
+		</>
+	);
 }
 
 export default App;
